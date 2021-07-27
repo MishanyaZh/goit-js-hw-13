b4daa1c66edadb6a86a811fbeb43de9b4d6d1f64
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-07-27 15:18:18 +0200

    fetchImageApi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+console.log('hw-13');
+
+
+
+const searchForm = document.querySelector('.search-form');
+console.log(searchForm);
+const galleryContainer = document.querySelector('.gallery');
+console.log(galleryContainer);
+const loadMoreBtn = document.querySelector('.load-more');
+console.log(loadMoreBtn);
---
+++ src/js/fetch-Image-Api.js
@@ -0,0 +1,37 @@
+import Notiflix from "notiflix";
+
+const API_URL = 'https://pixabay.com/api/';
+const API_KEY = '22670626-c734a5ab3fb2edefe4011dc83';
+
+// fetchImageApi
+export default class ImageService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    };
+
+
+    fetchImages() {
+        return fetch(`${API_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
+            .then(response => response.json())
+            .then(({ total, hits }) => {
+                if (total > 0 && this.page === 1) {
+                    Notiflix.Notify.success(`Hooray! We found ${total} images.`);
+                }
+                this.page += 1;
+                return hits;
+            })
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
